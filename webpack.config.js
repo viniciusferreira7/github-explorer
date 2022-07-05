f0a5593948a8df9c65e57eafd60726f0d4308173
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-05 19:49:17 -0300

    Utilizando o source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
 
+  devtool: 'eval-source-map',
+
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
 
   output: {
