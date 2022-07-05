14975504d8505124ce4356b7bf4d0e629f861c4b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-05 18:59:27 -0300

    Inserindo a configuração de devServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
   ],
+
   module: {
     rules: [
       {
